a5a013827baab3b8240dc844e515bdab5873b7e9
ellGoltso <goltsovi3@gmail.com>
2025-10-06 21:18:33 +0300

    create custom command

---
+++ catalog/management/commands/add_test_products.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = 'Add test products to the database'
+
+    def handle(self, *args, **options):
+
+        Category.objects.all().delete()
+        Category.objects.create(name='test_category', description='category description')
+        category = Category.objects.get(name='test_category')
+        Product.objects.create(name='test_product', description='tedt_desc', price=25000, category=category)
