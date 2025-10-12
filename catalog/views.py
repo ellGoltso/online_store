b82513f1104f7155c1892d6d2665b10d079c4519
ellGoltso <goltsovi3@gmail.com>
2025-10-12 09:38:07 +0300

    create subtemplate

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'index.html', context)
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
+
+# def list_products(request):
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, '')
