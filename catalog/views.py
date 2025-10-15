e7d956950e18444d064085f2c55565a42a8cfe45
ellGoltso <goltsovi3@gmail.com>
2025-10-15 06:10:10 +0300

    change product detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 def home(request):
@@ -10,11 +10,7 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
+    product = get_object_or_404(Product, id=product_id)
     context = {'product': product}
     return render(request, 'product_detail.html', context)
 
-# def list_products(request):
-#     products = Product.objects.all()
-#     context = {'products': products}
-#     return render(request, '')
