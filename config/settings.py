48ef508587f9d37cc78c256abfdbcf181946adce
ellGoltso <goltsovi3@gmail.com>
2025-10-15 09:16:29 +0300

    add CBV

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'catalog/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'catalog/templates/catalog')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
